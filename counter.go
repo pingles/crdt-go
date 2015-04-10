d04e490159eff5d8584d43b5d279c6d3ad5493bc
Paul Ingles <paul@oobaloo.co.uk>
2015-04-10 12:08:51 +0100

    add state decrement

--- counter.go
+++ counter.go
@@ -2,14 +2,19 @@ package crdt
 
 type Counter struct {
   nodeIdentity  string
-  counterValues map[string]int64
+
+  incValues     map[string]int64
+  decValues     map[string]int64
 }
 
 // retrieves the current value of the distributed counter
 func (c *Counter) Value() int64 {
   x := int64(0)
-  for _, value := range c.counterValues {
+  for _, value := range c.incValues {
     x = x + value
+  }
+  for _, value := range c.decValues {
+    x = x - value
   }
   return x
 }
@@ -17,20 +22,31 @@ func (c *Counter) Value() int64 {
 // called when we want to merge our counter with the state of the same
 // counter from another node.
 func (c *Counter) Merge(other *Counter) {
-  for key, value := range other.counterValues {
-    ourValue := c.counterValues[key]
+  for key, value := range other.incValues {
+    ourValue := c.incValues[key]
     if value > ourValue {
-      c.counterValues[key] = value
+      c.incValues[key] = value
+    }
+  }
+
+  for key, value := range other.decValues {
+    ourValue := c.decValues[key]
+    if value > ourValue {
+      c.decValues[key] = value
     }
   }
 }
 
 func (c *Counter) Increment() {
-  c.counterValues[c.nodeIdentity] += 1
+  c.incValues[c.nodeIdentity] += 1
+}
+
+func (c *Counter) Decrement() {
+  c.decValues[c.nodeIdentity] += 1
 }
 
 // creates a new counter, each node that has a replica of the counter
 // state must have a unique identity
 func NewCounter(nodeIdentity string) *Counter {
-  return &Counter{nodeIdentity, make(map[string]int64)}
+  return &Counter{nodeIdentity, make(map[string]int64), make(map[string]int64)}
 }
--- counter_test.go
+++ counter_test.go
@@ -15,6 +15,26 @@ func TestCounterIncrement(t *testing.T) {
   }
 }
 
+func TestCounterDecrement(t *testing.T) {
+  c1 := NewCounter("node1")
+  c1.Increment()
+  c1.Decrement()
+  
+  if c1.Value() != 0 {
+    t.Error("should have 0 value, was", c1.Value())
+  }
+  
+  c2 := NewCounter("node2")
+  c2.Increment()
+  c2.Increment()
+  c2.Decrement()
+  
+  c1.Merge(c2)
+  if c1.Value() != 1 {
+    t.Error("should have converged to value of 1, was", c1.Value())
+  }
+}
+
 func TestCounterMerge(t *testing.T) {
   c1 := NewCounter("node1")
   c2 := NewCounter("node2")
