38c36d87ed1818cb99b62dae38601915b0f8e23f
Paul Ingles <paul@oobaloo.co.uk>
2015-04-13 11:17:55 +0100

    rename nodeIdentity to processIdentity; run go fmt

--- counter.go
+++ counter.go
@@ -1,52 +1,52 @@
 package crdt
 
 type Counter struct {
-  nodeIdentity  string
+	processIdentity string
 
-  incValues     map[string]int64
-  decValues     map[string]int64
+	incValues map[string]int64
+	decValues map[string]int64
 }
 
 // retrieves the current value of the distributed counter
 func (c *Counter) Value() int64 {
-  x := int64(0)
-  for _, value := range c.incValues {
-    x = x + value
-  }
-  for _, value := range c.decValues {
-    x = x - value
-  }
-  return x
+	x := int64(0)
+	for _, value := range c.incValues {
+		x = x + value
+	}
+	for _, value := range c.decValues {
+		x = x - value
+	}
+	return x
 }
 
 // called when we want to merge our counter with the state of the same
 // counter from another node.
 func (c *Counter) Merge(other *Counter) {
-  for key, value := range other.incValues {
-    ourValue := c.incValues[key]
-    if value > ourValue {
-      c.incValues[key] = value
-    }
-  }
+	for key, value := range other.incValues {
+		ourValue := c.incValues[key]
+		if value > ourValue {
+			c.incValues[key] = value
+		}
+	}
 
-  for key, value := range other.decValues {
-    ourValue := c.decValues[key]
-    if value > ourValue {
-      c.decValues[key] = value
-    }
-  }
+	for key, value := range other.decValues {
+		ourValue := c.decValues[key]
+		if value > ourValue {
+			c.decValues[key] = value
+		}
+	}
 }
 
 func (c *Counter) Increment() {
-  c.incValues[c.nodeIdentity] += 1
+	c.incValues[c.processIdentity] += 1
 }
 
 func (c *Counter) Decrement() {
-  c.decValues[c.nodeIdentity] += 1
+	c.decValues[c.processIdentity] += 1
 }
 
 // creates a new counter, each node that has a replica of the counter
 // state must have a unique identity
-func NewCounter(nodeIdentity string) *Counter {
-  return &Counter{nodeIdentity, make(map[string]int64), make(map[string]int64)}
+func NewCounter(processIdentity string) *Counter {
+	return &Counter{processIdentity, make(map[string]int64), make(map[string]int64)}
 }
