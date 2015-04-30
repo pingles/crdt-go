7888e2b45f279aa380ec583f905f860a7b369cc8
Paul Ingles <paul@oobaloo.co.uk>
2015-04-30 13:22:54 +0100

    add some locks

--- op_counter.go
+++ op_counter.go
@@ -1,77 +1,99 @@
 package crdt
 
+import (
+	"sync"
+)
+
 type OpCounter struct {
-  processIdentity string
-  value           int64
-  
-  remoteReplicas  []chan<- CounterOperation
+	processIdentity string
+	value           int64
+
+	remoteReplicas []chan<- CounterOperation
+
+	sync.RWMutex
 }
 
-type CounterOperation interface {
-  Perform(*OpCounter)
-}
-type IncrementOperation struct {
-  value int64
-}
-type DecrementOperation struct {
-  value int64
-}
-func IncrementByOne() *IncrementOperation {
-  return &IncrementOperation{1}
-}
-func (op *IncrementOperation) Perform(counter *OpCounter) {
-  counter.inc(op.value)
-}
 func (c *OpCounter) inc(x int64) {
-  c.value += x
+	c.Lock()
+	defer c.Unlock()
+	c.value += x
 }
 
-func (op *DecrementOperation) Perform(counter *OpCounter) {
-  counter.dec(op.value)
-}
-
-func DecrementByOne() *DecrementOperation {
-  return &DecrementOperation{1}
-}
 func (c *OpCounter) dec(x int64) {
-  c.value -= x
+	c.Lock()
+	defer c.Unlock()
+	c.value -= x
 }
 
 func NewOpCounter(processIdentity string) *OpCounter {
-  replicas := make([]chan<- CounterOperation, 0)
-  return &OpCounter{processIdentity: processIdentity, remoteReplicas: replicas}
+	replicas := make([]chan<- CounterOperation, 0)
+	return &OpCounter{processIdentity: processIdentity, remoteReplicas: replicas}
+}
+
+type CounterOperation interface {
+	Perform(*OpCounter)
+}
+type IncrementOperation struct {
+	value int64
+}
+type DecrementOperation struct {
+	value int64
+}
+
+func IncrementByOne() *IncrementOperation {
+	return &IncrementOperation{1}
+}
+func (op *IncrementOperation) Perform(counter *OpCounter) {
+	counter.inc(op.value)
+}
+
+func (op *DecrementOperation) Perform(counter *OpCounter) {
+	counter.dec(op.value)
+}
+
+func DecrementByOne() *DecrementOperation {
+	return &DecrementOperation{1}
 }
 
 func (c *OpCounter) Value() int64 {
-  return c.value
+	c.RLock()
+	defer c.RUnlock()
+
+	return c.value
 }
 
 func (c *OpCounter) Increment() {
-  op := IncrementByOne()
-  op.Perform(c)
-  c.replicate(op)
+	op := IncrementByOne()
+	op.Perform(c)
+	c.replicate(op)
 }
 
 func (c *OpCounter) Decrement() {
-  op := DecrementByOne()
-  op.Perform(c)
-  c.replicate(op)
+	op := DecrementByOne()
+	op.Perform(c)
+	c.replicate(op)
 }
 
 func (c *OpCounter) replicate(op CounterOperation) {
-  for _, replica := range c.remoteReplicas {
-    replica <- op
-  }
+	c.RLock()
+	defer c.RUnlock()
+
+	for _, replica := range c.remoteReplicas {
+		replica <- op
+	}
 }
 
 func (c *OpCounter) Listen(operations <-chan CounterOperation) {
-  go func() {
-    for op := range operations {
-      op.Perform(c)
-    }
-  }()
+	go func() {
+		for op := range operations {
+			op.Perform(c)
+		}
+	}()
 }
 
 func (c *OpCounter) AddReplica(replica chan<- CounterOperation) {
-  c.remoteReplicas = append(c.remoteReplicas, replica)
+	c.Lock()
+	defer c.Unlock()
+
+	c.remoteReplicas = append(c.remoteReplicas, replica)
 }
--- op_counter_test.go
+++ op_counter_test.go
@@ -1,50 +1,50 @@
 package crdt
 
 import (
-  "testing"
-  "time"
+	"testing"
+	"time"
 )
 
 func TestOpCounter(t *testing.T) {
-  c := NewOpCounter("node1")
-  if c.Value() != 0 {
-    t.Error("should have initial value of 0")
-  }
-  
-  c.Increment()
-  if c.Value() != 1 {
-    t.Error("should have incremented")
-  }
-  
-  c.Decrement()
-  if c.Value() != 0 {
-    t.Error("should have decremented")
-  }
+	c := NewOpCounter("node1")
+	if c.Value() != 0 {
+		t.Error("should have initial value of 0")
+	}
+
+	c.Increment()
+	if c.Value() != 1 {
+		t.Error("should have incremented")
+	}
+
+	c.Decrement()
+	if c.Value() != 0 {
+		t.Error("should have decremented")
+	}
 }
 
 func TestOpCounterReplica(t *testing.T) {
-  c1 := NewOpCounter("node1")
-  c2 := NewOpCounter("node2")
-  c3 := NewOpCounter("node3")
-  
-  node2ReplicaCh := make(chan CounterOperation)
-  c1.AddReplica(node2ReplicaCh)
-  c2.Listen(node2ReplicaCh)
+	c1 := NewOpCounter("node1")
+	c2 := NewOpCounter("node2")
+	c3 := NewOpCounter("node3")
 
-  node3ReplicaCh := make(chan CounterOperation)
-  c1.AddReplica(node3ReplicaCh)
-  c3.Listen(node3ReplicaCh)
-  
-  c1.Increment()
-  
-  // hacky for now but the replicas read using go channels
-  // so have to wait a little for read to be processed.
-  <-time.After(time.Second * 1)
-  
-  if c2.Value() != 1 {
-    t.Error("should have incremented node2 replica")
-  }
-  if c3.Value() != 1 {
-    t.Error("should have incremented node3 replica")
-  }
+	node2ReplicaCh := make(chan CounterOperation)
+	c1.AddReplica(node2ReplicaCh)
+	c2.Listen(node2ReplicaCh)
+
+	node3ReplicaCh := make(chan CounterOperation)
+	c1.AddReplica(node3ReplicaCh)
+	c3.Listen(node3ReplicaCh)
+
+	c1.Increment()
+
+	// hacky for now but the replicas read using go channels
+	// so have to wait a little for read to be processed.
+	<-time.After(time.Second * 1)
+
+	if c2.Value() != 1 {
+		t.Error("should have incremented node2 replica")
+	}
+	if c3.Value() != 1 {
+		t.Error("should have incremented node3 replica")
+	}
 }
